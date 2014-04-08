b67b810bccff4374bb2ea19ca3ab4184d0cf2ef5
Nethersoul <chrigi_ritz@hotmail.com>
2014-04-08 09:20:52 +0200

    t

--- Sketchball/Controls/PinballControl2.cs
+++ Sketchball/Controls/PinballControl2.cs
@@ -199,10 +199,6 @@ namespace Sketchball.Controls
                 }
             }
 
-            foreach (BoundingBox b in this.machine.Layout.Bounds.boundingBoxes)
-            {
-                b.drawDEBUG(g, Pens.Red);
-            }
 
             foreach (Ball ball in this.machine.Balls)
             {
@@ -231,7 +227,7 @@ namespace Sketchball.Controls
 
             g.FillRectangle(Brushes.White, 0, 0, Width, Height);
             //g.DrawRectangle(Pens.Red, 0, 0, Width - 1, Height - 1);
-            machine.Layout.DrawBackground(g);
+           
             for (int y = 0; y < Height; y += 10)
             {
                 g.DrawLine(Pens.LightGray, 0, y, Width, y);
