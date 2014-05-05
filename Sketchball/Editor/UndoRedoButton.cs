46b5fe6e8cd36ac6f0eb93865cb1cd49e9ba1613
Simon Meer <simon.meer@bluewin.ch>
2014-05-05 18:44:59 +0200

    Amend last commit.

---
+++ Sketchball/Editor/UndoRedoButton.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sketchball.Editor
+{
+
+
+    public class UndoButton : ToolStripButton
+    {
+        private History history;
+
+        public UndoButton(History history)
+        {
+            this.history = history;
+            history.Change += OnChange;
+            this.Image = Properties.Resources.Undo_icon;
+
+            OnChange();
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            history.Undo();
+        }
+
+        void OnChange()
+        {
+            Enabled = history.CanUndo();
+        }
+
+    }
+
+    public class UndoItem : ToolStripMenuItem
+    {
+        private History history;
+
+        public UndoItem(History history)
+        {
+            this.history = history;
+            history.Change += OnChange;
+            this.Image = Properties.Resources.Undo_icon;
+            this.Text = "Undo";
+            this.ShortcutKeys = Keys.Control | Keys.Z;
+            OnChange();
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            history.Undo();
+        }
+
+        void OnChange()
+        {
+            Enabled = history.CanUndo();
+        }
+
+    }
+
+
+    public class RedoButton : ToolStripButton
+    {
+        private History history;
+
+        public RedoButton(History history)
+        {
+            this.history = history;
+            history.Change += OnChange;
+            this.Image = Properties.Resources.Redo_icon;
+
+            OnChange();
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            history.Redo();
+        }
+
+        void OnChange()
+        {
+            Enabled = history.CanRedo();
+        }
+
+    }
+
+    public class RedoItem : ToolStripMenuItem
+    {
+        private History history;
+
+        public RedoItem(History history)
+        {
+            this.history = history;
+            history.Change += OnChange;
+            this.Image = Properties.Resources.Redo_icon;
+            this.Text = "Redo";
+            this.ShortcutKeys = Keys.Control | Keys.Y;
+            OnChange();
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            history.Redo();
+        }
+
+        void OnChange()
+        {
+            Enabled = history.CanRedo();
+        }
+
+    }
+}
