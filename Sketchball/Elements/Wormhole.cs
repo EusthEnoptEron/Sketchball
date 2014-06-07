3f6a05559d6e28a2b9334da7f223bd11570aa1dc
Nethersoul <chrigi_ritz@hotmail.com>
2014-06-07 14:47:51 +0200

    forgot class Wormhole.cs

---
+++ Sketchball/Elements/Wormhole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketchball.Elements
+{
+    public abstract class Wormhole : PinballElement
+    {
+        public static WormholeEntry WormholeEntryPending { get; set; }
+        public static WormholeExit WormholeExitPending { get; set; }
+
+    }
+}
